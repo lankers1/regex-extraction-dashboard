77fca5342e087a29e6e8a9c49c0863877beba545
Lankers1 <james.ciaramella@gmail.com>
2025-07-07 12:01:44 +0100

    feature - styling for tab components and appropriate ARIA roles

--- src/components/ui/Tabs/tabs.tsx
+++ src/components/ui/Tabs/tabs.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ReactNode, useState } from "react";
+import styles from "./styles.module.css";
 
 interface TabsProps {
   buttonLabels: string[];
@@ -10,17 +11,18 @@ export const Tabs = ({ buttonLabels, content }: TabsProps) => {
   const [selectedTab, setSelectedTab] = useState(0);
   return (
     <div>
-      <div>
+      <div role="tablist" className={styles.tabs}>
         {buttonLabels.map((label, index) => (
           <button
             key={"tab" + label + index}
             onClick={() => setSelectedTab(index)}
+            className={styles.tab}
           >
             {label}
           </button>
         ))}
       </div>
-      {content[selectedTab]}
+      <div role="tabpanel">{content[selectedTab]}</div>
     </div>
   );
 };
