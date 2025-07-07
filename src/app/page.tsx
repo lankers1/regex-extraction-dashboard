e1b63091d7ac68ba31facb275638bca60ae5fc3d
Lankers1 <james.ciaramella@gmail.com>
2025-07-07 17:57:25 +0100

    cleanup - remove unnecessary component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 import { Drawer } from "@/components/ui/Drawer";
-
-import styles from "./page.module.css";
-import { DrawerContent } from "./_components/DrawerContent";
+import { Tabs } from "@/components/ui/Tabs";
 import { TextContent } from "./_components/TextContent";
+import { ApprovalSection } from "./_components/ApprovalSection";
+import { EditSection } from "./_components/EditSection";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Drawer>
-        <DrawerContent />
+        <Tabs
+          buttonLabels={["Approval Mode", "Edit Mode"]}
+          content={[
+            <ApprovalSection key="approvalSection" />,
+            <EditSection key="editSection" />,
+          ]}
+        />
       </Drawer>
       <TextContent />
     </main>
