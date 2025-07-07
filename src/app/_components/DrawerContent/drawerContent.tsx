acc471440cbe83a6fa7e245bfaa4465931118542
Lankers1 <james.ciaramella@gmail.com>
2025-07-07 16:07:30 +0100

    feature - render ApprovalSection in drawer

--- src/app/_components/DrawerContent/drawerContent.tsx
+++ src/app/_components/DrawerContent/drawerContent.tsx
@@ -1,11 +1,12 @@
 import { Tabs } from "@/components/ui/Tabs";
 import { EditSection } from "../EditSection";
+import { ApprovalSection } from "../ApprovalSection";
 
 export const DrawerContent = () => {
   return (
     <Tabs
       buttonLabels={["Approval Mode", "Edit Mode"]}
-      content={[<div>hello</div>, <EditSection />]}
+      content={[<ApprovalSection />, <EditSection />]}
     />
   );
 };
